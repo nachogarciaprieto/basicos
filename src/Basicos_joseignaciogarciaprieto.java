31e441f6e879f0f49d1614cf90507b9f991f3a3d
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2018-10-28 21:34:29 +0100

    Añadidos varios tipos de datos y arreglado estéticamente.
    (en la rama numericos primer commit)

--- src/Basicos_joseignaciogarciaprieto.java
+++ src/Basicos_joseignaciogarciaprieto.java
@@ -20,16 +20,27 @@ public class Basicos_joseignaciogarciaprieto {
         long duplicaEntero;
         float grande;
         double doble;
+        boolean verdafalso;
 
         //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
+        grande = 3;
+        duplicaEntero = 56484984;
+        chiquitin = 32;
+        verdafalso = true;
 
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
-        System.err.println("Valor entero");
+        System.out.println("Valor entero");
         System.out.println(entero);
-        System.out.println("Valor doble");
-        System.out.println(doble);
+        System.out.println("Valor de grande");
+        System.out.println(grande);
+        System.out.println("Valor de duplicaEntero");
+        System.out.println(duplicaEntero);
+        System.out.println("Valor chiquitin");
+        System.out.println(chiquitin);
+        System.out.println("Valor verdafalso");
+        System.out.println(verdafalso);
     }
 //Para que cada uno de los métodos se ejecute y muestre el resultado, es necesario que realices una llamada a estos desde el método main.
 
