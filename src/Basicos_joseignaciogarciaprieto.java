88d15de60023ee12b307e5f81d0fb5a79284b7a5
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2018-10-28 22:16:10 +0100

    Creado un metodo main para imprimir en pantalla texto por medio de println.
    (en la rama cadenas primer commit)

--- src/Basicos_joseignaciogarciaprieto.java
+++ src/Basicos_joseignaciogarciaprieto.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author nacho
- */
+
 public class Basicos_joseignaciogarciaprieto {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+  
+
+ public static void main(String[] args) {
+     System.out.println("esto es un texto");
+  
+	   
+ 
+ }
+             
 }
