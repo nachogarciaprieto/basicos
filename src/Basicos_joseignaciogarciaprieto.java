b2e5596b4c3c2d4ac9a66cfb6afd32124292fea9
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2018-10-28 20:16:33 +0100

    Copiado el código de ejemplo que hay en el ejercicio.
    Para ir modificando en cada rama.

--- src/Basicos_joseignaciogarciaprieto.java
+++ src/Basicos_joseignaciogarciaprieto.java
@@ -10,11 +10,32 @@
  */
 public class Basicos_joseignaciogarciaprieto {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
     }
-    
+//Para que cada uno de los métodos se ejecute y muestre el resultado, es necesario que realices una llamada a estos desde el método main.
+
+ public static void main(String[] args) {
+        numericos();  // Llamada al método
+    }
+ 
+
 }
